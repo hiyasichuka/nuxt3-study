a4c5e0441a7879b6740cb060d838c4ef3687beaa
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-01-15 07:08:25 +0900

    update Hero

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -29,9 +29,12 @@
         bg-[url('https://carconfigurator.ferrari.com/assets/cars/portofinom/packages/default/car-ferrari-portofino-m_splash.jpg')]
       "
     >
-      <div
-        class="z-10 absolute w-full h-full flex justify-center items-center"
-      ></div>
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
     </div>
     <!-- HERO -->
   </div>
